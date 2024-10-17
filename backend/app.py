5ef8f7656f3021b1ae987dfbd07526ad4fa1b576
Grace Kim <gk3261@princeton.edu>
2024-10-17 10:28:48 -0400

    just flask part, no DB

--- backend/app.py
+++ backend/app.py
@@ -1,27 +1,19 @@
-import os
-from flask import Flask, render_template
-import SQLAlchemy
+from flask import Flask, render_template, make_response
+# import database # for some reason not loaded and not installable by pip3?
 from dotenv import load_dotenv
 load_dotenv() # load vars in .env file
 
 app = Flask(__name__, template_folder='../frontend')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-
-db = SQLAlchemy(app)
-
-# initialize db based on schema -- I need a way to check if this one's right though
-def init_db():
-    with app.app_context():
-        with open('schema.sql', 'r') as f:
-            db.engine.execute(f.read())
 
 # Welcome page route
-@app.route('/')
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
 def index():
-    return render_template('index.html')
+    html_code = render_template('index.html')
+    response = make_response(html_code)
+    return response
 
 # We need a CAS login route here that redirects to home page (these other pages should be separate file?)
 
 if __name__ == "__main__":
-    init_db()
     app.run(debug=True)
