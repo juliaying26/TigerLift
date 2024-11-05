d8c6f87ed5b3c035155a789adf3625b7ca22e074
Grace Kim <gk3261@princeton.edu>
2024-11-05 16:10:51 -0500

    run npm install antd to get default datetimepicker by antd

---
+++ frontend/src/components/MyDateTimePicker.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { DatePicker, TimePicker } from 'antd';
+import 'antd/dist/antd.css';
+
+export default function MyDateTimePicker() {
+  const [date, setDate] = useState(null);
+  const [time, setTime] = useState(null);
+
+  const handleDateChange = (value) => {
+    setDate(value);
+    console.log("Selected Date:", value ? value.format('YYYY-MM-DD') : null);
+  };
+
+  const handleTimeChange = (value) => {
+    setTime(value);
+    console.log("Selected Time:", value ? value.format('HH:mm:ss') : null);
+  };
+
+  return (
+    <div>
+      <DatePicker
+        onChange={handleDateChange}
+        placeholder="Select date"
+        style={{ marginRight: '8px' }}
+      />
+      <TimePicker
+        onChange={handleTimeChange}
+        placeholder="Select time"
+      />
+    </div>
+  );
+}
