06d3a9e5965a57969048248948ed1407c1277aee
Grace Kim <gk3261@princeton.edu>
2024-11-06 11:39:11 -0500

    one svg added

--- frontend/src/components/IconButton.jsx
+++ frontend/src/components/IconButton.jsx
@@ -5,8 +5,9 @@ export default function IconButton({ type, onClick, disabled, className }) {
         switch (type) {
             case 'back':
                 return (
-                    <svg>
-                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+</svg>
                 );
             case 'checkmark':
                 return (
