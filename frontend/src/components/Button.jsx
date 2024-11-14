4f52ca398230cc5962446680b86af7c0c99bf21b
Julia Ying <juliaying@princeton.edu>
2024-11-14 09:42:08 -0500

    adjusted colors and added button status per request status

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 
-export default function Button({ onClick, disabled, className, children }) {
+export default function Button({
+  onClick,
+  disabled,
+  className,
+  children,
+  status = "",
+}) {
+  let buttonStyling = "";
+  if (status === "pending") {
+    buttonStyling = "bg-theme_light_1 text-theme_dark_1";
+  } else if (status === "accepted") {
+    buttonStyling = "bg-theme_light_2 text-theme_dark_2";
+  }
   return (
     <button
       onClick={onClick}
       disabled={disabled}
-      className={`px-2 py-1 rounded-md ${className}`}
+      className={`px-2 py-1 rounded-md ${className} ${buttonStyling}`}
     >
       {children}
     </button>
--- frontend/src/components/RideCard.jsx
+++ frontend/src/components/RideCard.jsx
@@ -6,13 +6,18 @@ export default function RideCard({
   buttonText,
   buttonOnClick,
   buttonClassName,
+  buttonStatus = "",
 }) {
   return (
     <div id="ridecard" className="p-6 bg-white rounded-lg h-full">
       <div className="flex flex-col gap-2 justify-between h-full">
         <div>{children}</div>
         <div className="self-end">
-          <Button onClick={buttonOnClick} className={buttonClassName}>
+          <Button
+            onClick={buttonOnClick}
+            className={buttonClassName}
+            status={buttonStatus}
+          >
             {buttonText}
           </Button>
         </div>
